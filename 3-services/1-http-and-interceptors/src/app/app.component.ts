42173b98897b10abd3364bd6e28330530313c9e5
TheSpaceHuman <kolodyazhnyartem@gmail.com>
2020-04-24 12:08:58 +0300

    module 3 done

--- 3-services/1-http-and-interceptors/src/app/app.component.ts
+++ 3-services/1-http-and-interceptors/src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { IProduct, ProductsService } from './products.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  public products: IProduct[] = [];
+  public products$!: Observable<IProduct[]>;
+  public title = '3. Cервисы и Http протокол';
+  constructor(private productsService: ProductsService) {}
 
+  ngOnInit() {
+    this.products$ = this.productsService.getProducts();
+  }
+  public addProduct(_product: IProduct) {}
+}
--- 3-services/1-http-and-interceptors/src/app/interceptor.service.ts
+++ 3-services/1-http-and-interceptors/src/app/interceptor.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
+import { filter, map } from 'rxjs/operators';
+import { BASE_URL_TOKEN } from './config';
+import { Observable } from 'rxjs';
 
 export interface IRes {
   data: any;
   error?: string;
 }
 @Injectable()
-export class InterceptorService {}
+export class InterceptorService implements HttpInterceptor {
+  constructor(@Inject(BASE_URL_TOKEN) private _baseUrl: string) {}
+
+  public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpResponse<any>> {
+    const headers: HttpHeaders = req.headers.append('Content-Type', 'application/json');
+    const jsonReq: HttpRequest<any> = req.clone({
+      headers,
+      url: `${this._baseUrl}${req.url}`,
+    });
+
+    return next.handle(jsonReq).pipe(
+      filter(this._isHttpResponse),
+      map((res: HttpResponse<IRes>) => {
+        return res.clone({ body: res.body && res.body.data });
+      })
+    );
+  }
+
+  private _isHttpResponse(event: HttpEvent<any>): event is HttpResponse<IRes> {
+    if (event instanceof HttpResponse) {
+      return true;
+    }
+    return false;
+  }
+
+}
